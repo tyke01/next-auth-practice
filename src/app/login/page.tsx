cac8d16ac03c87f04d1da8c75e9b6f35144e1e65
tyke01 <techvictor653@gmail.com>
2025-06-22 12:14:47 +0300

    feat: add login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,72 @@
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { IconBrandGithub, IconBrandGoogle } from "@tabler/icons-react";
+import Link from "next/link";
+
+const LoginPage = () => {
+  return (
+    <div className="mt-10 max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white border border-[#121212]  dark:bg-black">
+      <form action="" className="my-8 space-y-2">
+        <div className="flex flex-col gap-2">
+          <Label htmlFor="email">Email Address</Label>
+          <Input
+            id="email"
+            placeholder="projectmayhem@fc.com"
+            type="email"
+            name="email"
+          />
+        </div>
+        <div className="flex flex-col gap-2">
+          <Label htmlFor="email">Password</Label>
+          <Input
+            id="password"
+            placeholder="*************"
+            type="password"
+            name="password"
+            className="mb-6"
+          />
+        </div>
+
+        <button className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset] cursor-pointer">
+          Login &rarr;
+        </button>
+
+        <p className="text-right text-neutral-600 text-sm max-w-sm mt-4 dark:text-neutral-300">
+          Don't have account?{" "}
+          <Link href="/register" className="hover:underline">
+            Register
+          </Link>
+        </p>
+
+        <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
+
+        <section className="flex flex-col space-y-4">
+          <form action="">
+            <button
+              className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)] cursor-pointer"
+              type="submit"
+            >
+              <IconBrandGithub className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
+              <span className="text-neutral-700 dark:text-neutral-300 text-sm">
+                Github
+              </span>
+            </button>
+          </form>
+          <form action="">
+            <button
+              className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)] cursor-pointer"
+              type="submit"
+            >
+              <IconBrandGoogle className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
+              <span className="text-neutral-700 dark:text-neutral-300 text-sm">
+                Google
+              </span>
+            </button>
+          </form>
+        </section>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
