036a7955c20cb7b05697089081be798d8cb26575
tyke01 <techvictor653@gmail.com>
2025-06-23 11:44:42 +0300

    feat: add password hashing for user registration

--- src/actions/user.ts
+++ src/actions/user.ts
@@ -1,39 +1,42 @@
-"use server"
+"use server";
 
 import connectDB from "@/lib/db";
 import { User } from "@/models/user";
 import { redirect } from "next/navigation";
+import { hash } from "bcryptjs";
 
 
-const register  = async (formData: FormData) => {
-    const firstName = formData.get("firstname") as string;
-    const lastName = formData.get("lastname") as string;
-    const email = formData.get("email") as string;
-    const password = formData.get("password") as string;
 
-    if(!firstName || !lastName || !email || !password) {
-        throw new Error("All fields are required");
-    }
+const register = async (formData: FormData) => {
+  const firstName = formData.get("firstname") as string;
+  const lastName = formData.get("lastname") as string;
+  const email = formData.get("email") as string;
+  const password = formData.get("password") as string;
 
-    await connectDB()
+  if (!firstName || !lastName || !email || !password) {
+    throw new Error("All fields are required");
+  }
 
-    // existing user check
-    const existingUser = await User.findOne({email});
-    if(existingUser) {
-        throw new Error("User already exists");
-    }
+  await connectDB();
 
-    await User.create({
-        firstName,
-        lastName,
-        email,
-        password
-    })
+  // existing user check
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    throw new Error("User already exists");
+  }
 
-    console.log("User registered successfully");
+  const hashedPassword = await hash(password, 12);
 
-    redirect("/login");
+  await User.create({
+    firstName,
+    lastName,
+    email,
+    password: hashedPassword,
+  });
 
-}
+  console.log("User registered successfully");
 
-export {register}
+  redirect("/login");
+};
+
+export { register };
